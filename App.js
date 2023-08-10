785ef9eab0da2313abe921e91ea401e1d7ade970
gamerfoxx <admin@josephludbrook.com>
2023-08-10 17:16:06 -0400

    begin todo list

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StyleSheet, Text, View, Button } from 'react-native'; //StyleSheet = styles, Text = added text, View = div
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'; //StyleSheet = styles, Text = added text, View = div
 //full list found https://reactnative.dev/docs/intro-react-native-components
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <Text>More text</Text>
-      <Button title="click me" />
-    </View>
-  );
+	return (
+		<View style={styles.appContainer}>
+			<View>
+				<TextInput placeholder="To Do" />
+				<Button title="Add To Do item" />
+			</View>
+			<View>
+				<Text>To do</Text>
+			</View>
+		</View>
+	);
 }
 
+//not all CSS styles exist in react native
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	appContainer: {
+		padding: 50,
+	},
 });
